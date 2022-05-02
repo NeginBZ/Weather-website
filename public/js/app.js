8b3cdd479b3c2385c5e41c5cb9ba51864086641b
negin <nbeheshtizavare3698@conestogac.on.ca>
2022-05-02 12:38:43 -0400

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ weatherForm.addEventListener(('submit'),(e) =>{
     message.textContent = 'loading.......'
     error.textContent = ''
 
-    fetch('http://localhost:3000/weather?address='+search.value).then((response) => {
+    fetch('/weather?address='+search.value).then((response) => {
     response.json().then((data) => {
         if(data.error){
             message.textContent = ''
